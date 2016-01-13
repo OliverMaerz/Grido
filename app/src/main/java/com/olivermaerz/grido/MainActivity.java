8831db6434e7750817f5ca41cd0c1550e14f6e0c
Oliver Maerz <github@olivermaerz.com>
2016-01-13 20:42:00 -0300

    Load grid fragment dynamically

--- app/src/main/java/com/olivermaerz/grido/MainActivity.java
+++ app/src/main/java/com/olivermaerz/grido/MainActivity.java
@@ -26,6 +26,29 @@ public class MainActivity extends AppCompatActivity implements GridFragment.OnFr
             }
         });
         */
+
+        // Check that the activity is using the layout version with
+        // the fragment_container FrameLayout
+        if (findViewById(R.id.fragment_container) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            // Create a new Fragment to be placed in the activity layout
+            GridFragment gridFragment = new GridFragment();
+
+            // In case this activity was started with special instructions from an
+            // Intent, pass the Intent's extras to the fragment as arguments
+            gridFragment.setArguments(getIntent().getExtras());
+
+            // Add the fragment to the 'fragment_container' FrameLayout
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, gridFragment).commit();
+        }
     }
 
     @Override
